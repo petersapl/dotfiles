ee379d4967e3d92ee17973a41782aa6267a155a8
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2017-12-09 01:03:35 +0100

    Add start script and empty templates.

---
+++ src/scripts/init.ps1
@@ -0,0 +1,25 @@
+#create folder if not exists
+mkdir -Force C:\dotfiles | Out-Null
+
+#download DotfilesWrapper
+Write-Output "Downloading DotfilesWrapper..."
+Invoke-Expression ".\download-artifact.ps1"
+
+#extract DotfilesWrapper
+Write-Output "Extracting DotfilesWrapper..."
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::ExtractToDirectory("C:\dotfiles\dotfiles.zip", "C:\dotfiles")
+
+Remove-Item "C:\dotfiles\dotfiles.zip"
+
+#copy YAML files
+Get-ChildItem -Path "..\templates" -Recurse | Copy-Item -Destination "C:\dotfiles"
+
+#install Chocolatey
+Write-Output "Installing Chocolatey..."
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#start process
+Write-Output "Invoking DotfilesWrapper..."
+Invoke-Expression "C:\dotfiles\DotfilesWrapper.exe C:\dotfiles\choco.yaml C:\dotfiles\commands.yaml"
