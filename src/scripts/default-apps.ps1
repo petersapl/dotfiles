ab8997b42d8705f9e5b35030394af0fdcb8a3e1c
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2018-03-24 21:37:06 +0100

    enable WSL and Hyper-V.

--- src/scripts/default-apps.ps1
+++ src/scripts/default-apps.ps1
@@ -35,6 +35,12 @@ ForEach($appId in
 # Uninstall Windows Media Player.
 Disable-WindowsOptionalFeature -Online -FeatureName "WindowsMediaPlayer" -NoRestart -WarningAction SilentlyContinue | Out-Null
 
+#enable WSL
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart -WarningAction SilentlyContinue | Out-Null
+
+#enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart -WarningAction SilentlyContinue | Out-Null
+
 # Prevent “Suggested Applications” from returning.
 if (!(Test-Path "HKLM:\Software\Policies\Microsoft\Windows\CloudContent")) {
   New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" -Type Folder | Out-Null
