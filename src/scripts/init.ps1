ab05649be03291495aa72c7f25854494a6af0325
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2018-03-24 21:26:21 +0100

    Disable UAC.

--- src/scripts/init.ps1
+++ src/scripts/init.ps1
@@ -6,6 +6,16 @@ $downloadLocation = [System.IO.Path]::GetTempPath() + "dotfiles"
 #create folder in TEMP path if not exists
 mkdir -Force $downloadLocation | Out-Null
 
+#download YAML files
+Write-Output "Downloading YAML files..."
+Invoke-RestMethod "https://raw.githubusercontent.com/DiXN/dotfiles/master/src/templates/choco.yaml" | Out-File "choco.yaml"
+Invoke-RestMethod "https://raw.githubusercontent.com/DiXN/dotfiles/master/src/templates/commands.yaml" | Out-File "commands.yaml"
+
+#download scripts
+Write-Output "Downloading script files..."
+Invoke-RestMethod "https://raw.githubusercontent.com/DiXN/dotfiles/master/src/scripts/default-apps.ps1" | Out-File "default-apps.ps1"
+Invoke-RestMethod "https://raw.githubusercontent.com/DiXN/dotfiles/master/src/scripts/download-artifact.ps1" | Out-File "download-artifact.ps1"
+
 #download DotfilesWrapper
 Write-Output "Downloading DotfilesWrapper..."
 Invoke-Expression ".\download-artifact.ps1 -apiKey $apiKey"
@@ -17,14 +27,12 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 Remove-Item "$downloadLocation\dotfiles.zip"
 
-#download YAML files
-Write-Output "Downloading YAML files..."
-Invoke-RestMethod "https://raw.githubusercontent.com/DiXN/dotfiles/master/src/templates/choco.yaml" | Out-File "choco.yaml"
-Invoke-RestMethod "https://raw.githubusercontent.com/DiXN/dotfiles/master/src/templates/commands.yaml" | Out-File "commands.yaml"
-
-#download scripts
-Write-Output "Downloading script files..."
-Invoke-RestMethod "https://raw.githubusercontent.com/DiXN/dotfiles/master/src/scripts/default-apps.ps1" | Out-File "default-apps.ps1"
+#disable UAC
+Write-Output 'Disabling UAC ...'
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value "0"
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorUser" -Value "0"
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Value "1"
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Value "0"
 
 #install Chocolatey
 Write-Output "Installing Chocolatey..."
